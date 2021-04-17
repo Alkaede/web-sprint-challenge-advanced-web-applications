7c6129aa5e06a8d819036c8623054e3fa7675ad9
Alkaede <andrewcast32@gmail.com>
2021-04-17 16:02:40 -0700

    checklist updated, bubblepage has on mount axios get call

--- src/components/BubblePage.js
+++ src/components/BubblePage.js
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from "react";
+import { axiosWithAuth } from "../helpers/axiosWithAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+
+  useEffect(() => {
+    axiosWithAuth().get(`/colors`)
+      .then(res => {
+        console.log('color stuff: ', res.data)
+        setColorList(res.data)
+      })
+      .catch(err => console.error('no colors: ', err))
+  }, [])
+
 
   return (
     <div className="container">
